cb309672a21e77e87d104cdc2f233bb55064a779
Chris-E <elhachemchris73@gmail.com>
2026-01-29 17:55:41 +1100

    Controls rework and new forces
    I kinda fixed the issue of the undesirable roll by giving the user two modes, one where the plane tends towards a neutral roll and one where the user has to directly control roll amongst other rotations. I also added a few other forces and torques to improve the feel and "accuracy" of the simulation

--- First Demo/simplified_torque_plane.gd
+++ First Demo/simplified_torque_plane.gd
@@ -4,7 +4,7 @@ extends RigidBody3D
 # https://www.youtube.com/watch?v=7vAHo2B1zLc
 # https://github.com/vazgriz/FlightSim
 
-var wind: Vector3 = Vector3.ZERO
+var wind: Vector3 = Vector3.RIGHT * 0
 
 var max_thrust: float = 5000
 
@@ -12,17 +12,34 @@ var min_drag_coefficient: float = 1
 var max_drag_coefficient: float = 3
 
 @export var lift_curve: Curve
-var base_angle_of_attack: float = 4
+var base_angle_of_attack: float = 4 # deg
 var lift_power: float = 30
 
+var yaw_force_power: float = 5
+
 var angular_drag_coefficient: float = 1
+
+var weathervane_torque_coefficient: float = 50
 
 var turning_speed: float = 1 # rad/s
 var steering_responsiveness: float = 50000
 
+var max_banking_angle: float = 60 # deg
+
+var anti_roll: bool = true
+
 func _physics_process(delta: float) -> void:
+	var yaw_input: float = Input.get_axis("yaw left", "yaw right")
+	var pitch_input: float = Input.get_axis("pitch down", "pitch up")
+	var roll_input: float = Input.get_axis("roll left", "roll right")
+	var thrust_input: float = Input.get_action_strength("thrust")
+	
+	if Input.is_action_just_pressed("anti-roll toggle"):
+		anti_roll = !anti_roll
+		print("Anti-Roll: " + str(anti_roll))
+	
 	var thrust_direction: Vector3 = -basis.z
-	var thrust_magnitude: float = Input.get_action_strength("thrust") * max_thrust
+	var thrust_magnitude: float = thrust_input * max_thrust
 	var thrust: Vector3 = thrust_magnitude * thrust_direction
 	apply_central_force(thrust)
 	
@@ -41,13 +58,36 @@ func _physics_process(delta: float) -> void:
 	var lift: Vector3 = lift_power * lift_coefficient * lift_air_flow.length_squared() * lift_direction
 	apply_central_force(lift)
 	
+	var yaw_force_direction: Vector3 = basis.x
+	var yaw_force_air_flow: Vector3 = global_basis.inverse() * local_air_velocity
+	yaw_force_air_flow.y = 0
+	var yaw_angle_of_attack: float = yaw_input * 6 + rad_to_deg(atan2(yaw_force_air_flow.x, yaw_force_air_flow.z))
+	var yaw_force_coefficient: float = lift_curve.sample(yaw_angle_of_attack)
+	var yaw_force: Vector3 = yaw_force_power * yaw_force_coefficient * yaw_force_air_flow.length_squared() * yaw_force_direction
+	apply_central_force(yaw_force)
+	
+	# This torque acts to reduce the angular velocity
 	var angular_drag: Vector3 = angular_drag_coefficient * angular_velocity.length_squared() * -angular_velocity.normalized()
 	apply_torque(angular_drag)
 	
-	var yaw_input: float = Input.get_axis("yaw left", "yaw right")
-	var pitch_input: float = Input.get_axis("pitch up", "pitch down")
-	var roll_input: float = Input.get_axis("roll left", "roll right")
-	var desired_angular_velocity: Vector3 = Vector3(-pitch_input, -yaw_input, -roll_input).normalized() * turning_speed 
-	var steering_error: Vector3 = desired_angular_velocity - global_basis.inverse() * angular_velocity
-	var steering_input: Vector3 = global_basis * steering_error * steering_responsiveness
-	apply_torque(steering_input)
+	# This torque pushes the plane to directly face air flow
+	var weathervane_torque: Vector3 = basis.z.cross(local_air_velocity.normalized()) * local_air_velocity.length_squared() * weathervane_torque_coefficient
+	apply_torque(weathervane_torque)
+	
+	var world_angular_velocity: Vector3 = global_basis.inverse() * angular_velocity
+	var desired_angular_velocity: Vector3
+	if anti_roll:
+		desired_angular_velocity = Vector3(pitch_input, 0, 0).normalized() * turning_speed 
+	else:
+		desired_angular_velocity = Vector3(pitch_input, -yaw_input, -roll_input).normalized() * turning_speed
+	var steering_error: Vector3 = desired_angular_velocity - world_angular_velocity
+	var steering_torque: Vector3 = global_basis * steering_error * steering_responsiveness
+	apply_torque(steering_torque)
+	
+	if anti_roll:
+		var banking_torque_direction: Vector3 = basis.z
+		var banking_angle: float = -yaw_input * max_banking_angle
+		var banking_error: float = deg_to_rad(banking_angle) - rotation.z
+		var banking_torque: Vector3 = banking_error * banking_torque_direction * 70000
+		apply_torque(banking_torque)
+	
